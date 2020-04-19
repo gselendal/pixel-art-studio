69dd2012effbf4c3996dd107f64240fbed66ee24
selen <selen@selens-MacBook-Pro.local>
2020-04-19 14:13:01 +0300

    initial

---
+++ code.py
@@ -0,0 +1,47 @@
+from PIL import Image, ImageFilter
+from scipy import misc, ndimage
+import matplotlib.pyplot as plt
+import imageio
+import math
+import numpy as np
+
+#face = misc.face()
+#imageio.imsave('face.png', face)
+face = imageio.imread('ben2.png')
+
+print(face.shape[1])
+
+
+def create_pixel_art(img):
+    _w = (img.shape[1])
+    w = _w
+    w = w - w % 10
+
+    _h = img.shape[0]
+    h = _h
+    h = h - h % 10
+    step_size = min(25, math.gcd(w, h))
+
+    cols = []
+
+    for i in range(_w):
+        print(i)
+        if (not(i % step_size < (step_size / 5))):
+            cols.append(i)
+
+    img = np.delete(img, cols, axis=1)
+
+    rows = []
+
+    for j in range(_h):
+        print(j)
+        if (not(j % step_size < (step_size / 5))):
+            rows.append(j)
+
+    img = np.delete(img, rows, axis=0)
+
+    return img
+
+
+pixel_face = create_pixel_art(face)
+imageio.imsave('pixel_face.png', pixel_face)
